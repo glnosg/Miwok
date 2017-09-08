1868c5bba05a69d2492a09dc0e13f7b098db76e9
glnosg <glina.osg@gmail.com>
2017-09-08 12:31:26 +0200

    Add ListView of numbers in Numbers category

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,13 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +16,26 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        ArrayList<String> numbersInEnglish = new ArrayList<>();
+
+        numbersInEnglish.add("One");
+        numbersInEnglish.add("Two");
+        numbersInEnglish.add("Three");
+        numbersInEnglish.add("Four");
+        numbersInEnglish.add("Five");
+        numbersInEnglish.add("Six");
+        numbersInEnglish.add("Seven");
+        numbersInEnglish.add("Eight");
+        numbersInEnglish.add("Nine");
+        numbersInEnglish.add("Ten");
+
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(NumbersActivity.this,
+                android.R.layout.simple_list_item_1, numbersInEnglish);
+
+        ListView listView = (ListView) findViewById(R.id.list);
+
+        listView.setAdapter(itemsAdapter);
+
     }
 }
